3e4e65320770f104ff5e64e442f41f49e64a7ac9
Oskar <oskar@hasinski.de>
2024-06-01 19:45:36 +0200

    event debugging

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,6 @@ import botocore
 def lambda_handler(event, context):
    print(f'boto3 version: {boto3.__version__}')
    print(f'botocore version: {botocore.__version__}')
+   print(event)
 
    return 'Github test 222'
